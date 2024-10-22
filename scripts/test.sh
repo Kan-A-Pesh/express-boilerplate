9aee9d5e56f03fbfd464ea129d9e83e25f216d8c
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-10-22 17:59:19 +0200

    feat: add tests to pipeline

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,5 @@
 set -e
 cd "$(dirname "$0")/.."
 
-docker compose -f docker-compose.test.yml up --build --force-recreate
+docker compose -f docker-compose.test.yml up -d --build --force-recreate
+docker compose -f docker-compose.test.yml attach test
