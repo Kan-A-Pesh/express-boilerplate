feed825152b20f48578a4550327d7ec73bc1ec7a
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-10-22 18:27:31 +0200

    feat(test): fixed test exit code

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-set -e
 cd "$(dirname "$0")/.."
 
 docker compose -f docker-compose.test.yml up -d --build --force-recreate
 docker compose -f docker-compose.test.yml attach test
+result=$?
+
+# Cleanup
+docker compose -f docker-compose.test.yml down --volumes -f > /dev/null 2>&1
+
+exit $result
